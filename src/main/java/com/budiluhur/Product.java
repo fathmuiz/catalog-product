544dfdc51010a045197d199833569d84155606d3
fathmuiz <187072380+fathmuiz@users.noreply.github.com>
2026-09-25 23:37:52 +0700

    add class Product

---
+++ src/main/java/com/budiluhur/Product.java
@@ -0,0 +1,22 @@
+package com.budiluhur.catalog.model;
+
+public class Product {
+    private String id;
+    private String name;
+    private double price;
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getterv& Setter
+    public String getId() { return id;}
+    public String getName() { return name;}
+    public double getPricce() { return price;}
+    @Override
+     public String to String() {
+         return "[" + id + "]" + name + " -Rp"  + price;
+     }
+}
